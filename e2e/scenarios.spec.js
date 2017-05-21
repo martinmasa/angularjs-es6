1fcdc5b48d35f7b6577eb19abf00f61fac7cb6ba
Martin M <mxa.code@gmail.com>
2017-05-21 20:22:21 +0100

    Fix protractor test.

--- e2e/scenarios.spec.js
+++ e2e/scenarios.spec.js
@@ -1,9 +1,9 @@
-describe('Demo App', () => {
+describe('AngularJS ES6 Demo App', () => {
   it('should display header', () => {
     browser.get('https://localhost:8080');
 
     element(by.tagName('h1')).getText().then((text) => {
-      expect(text).toEqual('App Component');
+      expect(text).toEqual('AngularJS ES6 Demo');
     });
   });
 });
